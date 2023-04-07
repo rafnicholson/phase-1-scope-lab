6cac4449cc523883a6c9dd3f962120c50178dcf8
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-06 19:19:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+
+var setBestCustomer = () =>  bestCustomer = 'not bob';
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'trevor';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'anthony';
+}
+
+
